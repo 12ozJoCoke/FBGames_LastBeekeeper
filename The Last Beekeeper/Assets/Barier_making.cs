b055cbb2bfc367053c3882ab2f299c63de74883e
12ozJoCoke <47345041+12ozJoCoke@users.noreply.github.com>
2021-04-19 10:44:19 -0700

    Matt's changes

---
+++ The Last Beekeeper/Assets/Barier_making.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barier_making : MonoBehaviour
+{
+    public int BarrierAmount, MaxAmount;
+    public GameObject BarrierPre;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BarrierAmount >= MaxAmount)
+        {
+            BarrierAmount = MaxAmount;
+        }
+        else if(BarrierAmount <= 0)
+        {
+            BarrierAmount = 0;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            if(BarrierAmount > 0)
+            {
+                Instantiate(BarrierPre);
+                BarrierAmount--;
+            }
+            else
+            {
+                Debug.Log("Ran out of barriers");
+            }
+           
+        }
+    }
+    
+}
---
+++ The Last Beekeeper/Assets/Scripts/BarrierHealth.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarrierHealth : MonoBehaviour
+{
+    public float nodamage, timeyboi;
+    public int BarrierHp, maxHP;
+    public bool youstilltakeadamage;
+
+    
+    
+    void Start()
+    {
+        BarrierHp = maxHP;
+        youstilltakeadamage = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+        if (!youstilltakeadamage)
+        {
+            if (timeyboi < nodamage)
+            {
+                timeyboi += Time.deltaTime;
+            }
+            else if(timeyboi >= nodamage)
+            {
+                timeyboi = 0;
+                youstilltakeadamage = false;
+            }
+        }
+        
+       if(BarrierHp == 0)
+        {
+            Destroy(gameObject);
+                
+            }
+    }
+    
+   
+    private void OnCollisionStay2D(Collision2D other)
+    {
+       if(other.gameObject.tag == "Enemy" && youstilltakeadamage)
+        {
+            BarrierHp--;
+            youstilltakeadamage = false;
+        }
+    }
+}
