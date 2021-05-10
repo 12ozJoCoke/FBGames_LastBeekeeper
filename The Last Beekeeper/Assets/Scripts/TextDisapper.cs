b2ceab920ed0f6a3b6fc9ea64db699429cda1ef7
1707226 <1707226@SC-VDGD-6QJ1853.local>
2021-05-10 10:15:32 -0700

    Finished level, added to UI and added tutoriel bits

---
+++ The Last Beekeeper/Assets/Scripts/TextDisapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisapper : MonoBehaviour
+{
+    public Text text;
+
+    public float timer;
+    public float timetoexit;
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer <= timetoexit)
+        {
+            
+        }
+
+        else if(timer >= timetoexit)
+        {
+            
+        }
+    }
+}
---
+++ The Last Beekeeper/Assets/Scripts/Tuto.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuto : MonoBehaviour
+{
+    public Canvas tutoriel;
+    public bool activate;
+    
+    void Start()
+    {
+        activate = true;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            activate = true;
+        }
+
+        if (!activate)
+        {
+            tutoriel.enabled = false;
+        }
+        else
+        {
+            tutoriel.enabled = true;
+        }
+
+        
+    }
+}
