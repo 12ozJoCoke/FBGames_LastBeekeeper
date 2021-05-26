5009db14fbe3491ebb67b6916d55f1f8f0f8f28c
MatthewRacela <1707226@mukilteo.wednet.edu>
2021-05-25 19:32:17 -0700

    Update PageFlip.cs, PageFlip.cs.meta, and 5 more files...

---
+++ The Last Beekeeper/Assets/PageFlip.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageFlip : MonoBehaviour
+{
+    public GameObject Image1;
+    public GameObject Image2;
+    public Canvas Can;
+    public bool SlideChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image1.active = true;
+        Image2.active = false;
+        Can.enabled = false;
+        SlideChange = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (!SlideChange)
+            {
+                SlideChange = true;
+            }
+            else
+            {
+                SlideChange = false;
+            }
+        }
+
+        if (SlideChange)
+        {
+            Image1.active = false;
+            Image2.active = true;
+            Can.enabled = true;
+        }
+        else
+        {
+            Image1.active = true;
+            Image2.active = false;
+            Can.enabled = false;
+        }
+    }
+}
